4851db6ed999181d5a97395951b67e79bab8b2e8
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2016-10-13 12:38:36 +0200

    Pep8.

--- backtranslate/__init__.py
+++ backtranslate/__init__.py
@@ -7,21 +7,17 @@ Copyright (c) 2015 Jeroen F.J. Laros <j.f.j.laros@lumc.nl>
 
 Licensed under the MIT license, see the LICENSE file.
 """
-
-
 from __future__ import (
     absolute_import, division, print_function, unicode_literals)
 from future.builtins import str, zip
 
 
-__version_info__ = ('0', '0', '5')
-
+__version_info__ = ('0', '1', '0')
 
 __version__ = '.'.join(__version_info__)
 __author__ = 'LUMC, Jeroen F.J. Laros'
 __contact__ = 'J.F.J.Laros@lumc.nl'
 __homepage__ = 'https://github.com/mutalyzer/backtranslate'
-
 
 usage = __doc__.split("\n\n\n")
 
